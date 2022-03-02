ec7f22a019a9d02b29e7934efe696d23296d68e2
Эдуард Беришев <Clair911@outlook.com>
2022-03-02 21:36:11 +0700

    Добавьте файлы проекта.

---
+++ 4sem_lb1(darts)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _4sem_lb1_darts_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score;
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                score = rand.Next(1,20);
+                Singleton.Instance.setScore(score);
+                Console.WriteLine("Игрок {0} получил {1} очков!", i%2, score);
+            }
+
+            Console.WriteLine("\n\nУ игрока №0 - {0} очков\n У игрока №1 - {1} очков\n",Singleton.Instance.sumUp(0),Singleton.Instance.sumUp(1));
+            if (Singleton.Instance.sumUp(0) > Singleton.Instance.sumUp(1))
+            {
+                Console.WriteLine("Выиграл игрок №0!");
+            }
+            else if (Singleton.Instance.sumUp(0) == Singleton.Instance.sumUp(1))
+            {
+                Console.WriteLine("Ничья!");
+            }
+            else
+            {
+                Console.WriteLine("Выиграл игрок №1!");
+            }
+        }
+    }
+}
---
+++ 4sem_lb1(darts)/Singletone.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4sem_lb1_darts_
+{
+    public sealed class Singleton
+    {
+        private static readonly Lazy<Singleton> instanceHolder =
+            new Lazy<Singleton>(() => new Singleton());
+
+        private List<int> score = new List<int>();
+
+        private Singleton()
+        {}
+
+        public void setScore(int points)
+        {
+            score.Add(points);
+        }
+
+        public int sumUp(int user)
+        {
+            int sum=0;
+            for (int i = user; i < score.Count; i+=2)
+            {
+                sum += score[i];
+            }
+
+            return sum;
+        }
+
+        public static Singleton Instance
+        {
+            get { return instanceHolder.Value; }
+        }
+    }
+}
